496c5398ee57c5046da6352c5cad0456d56e318f
patricia_ehrhardt <pehrhardt@epublishing.com>
2019-05-04 17:48:27 -0700

    Namespace replace of app with metronome. Create first class and function for the metronome. Test locally with Hello World.

---
+++ src/Metronome.js
@@ -0,0 +1,9 @@
+import React, { Component} from 'react';
+import './Metronome.css';
+
+class Metronome extends Component {
+  render() {
+    return <div className="metronome">HI!</div>;
+  }
+}
+export default Metronome;
