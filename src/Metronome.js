ec0591280f7496d9e5ccc7d879191b85a5a7516a
patricia_ehrhardt <pehrhardt@epublishing.com>
2019-05-04 18:23:56 -0700

    Add handler function to slider.

--- src/Metronome.js
+++ src/Metronome.js
@@ -12,17 +12,27 @@ class Metronome extends Component {
       beatsPerMeasure: 4
     };
   }
+  handleBpmChange = event => {
+    const bpm = event.target.value;
+    this.setState({ bpm });
+  }
 
   render() {
     const { playing, bpm } = this.state;
-    
+
     return (
       <div className="metronome">
         <div className="bpm-slider">
           <div>{bpm} BPM</div>
-          <input type="range" min="60" max="240" value={bpm} />
+          <input
+          type="range"
+          min="60"
+          max="240"
+          value={bpm}
+          onChange={this.handleBpmChange} />
         </div>
-        <button>{playing ? 'Stop' : 'Start'}</button>
+        <button>{playing ? 'Stop' : 'Start'}
+        </button>
       </div>
     );
   }
