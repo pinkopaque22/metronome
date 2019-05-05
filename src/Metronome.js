97cc892df1113bdd2a83154d6c5a47b8e9ca328c
patricia_ehrhardt <pehrhardt@epublishing.com>
2019-05-04 18:18:37 -0700

    Initialize state in the constructor.

--- src/Metronome.js
+++ src/Metronome.js
@@ -2,10 +2,20 @@ import React, { Component} from 'react';
 import './Metronome.css';
 
 class Metronome extends Component {
-  render() {
-    let bpm = 100;
-    let playing = false;
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      playing: false,
+      count: 0,
+      bpm: 100,
+      beatsPerMeasure: 4
+    };
+  }
+
+  render() {
+    const { playing, bpm } = this.state;
+    
     return (
       <div className="metronome">
         <div className="bpm-slider">
